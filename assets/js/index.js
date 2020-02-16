2f1389246b4f7d21c43c6f6ecf13782f14fc1ae2
Scott Kirkpatrick <scott.kirkpatrick@appian.com>
2020-02-16 15:58:44 -0500

    Upgrade Bulma and change navbar breakpoint

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,15 @@
 // Nav burger animation
-document.addEventListener("DOMContentLoaded", function () {
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener("click", function () {
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
-});
+$(document).ready(function() {
 
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+      return false;
+  });
+});
 // Bitty scrolling links script
 $("a[href^=\"#\"]").click(function(e) {
     e.preventDefault();
@@ -28,12 +20,3 @@ $("a[href^=\"#\"]").click(function(e) {
     return true;
 })
 
-// Modal closer
-$(".card").click(function () {
-    $($(this).attr("data-target")).addClass("is-active");
-    $("html").addClass("modal-open");
-});
-$(".modal-close").click(function () {
-    $($(this).attr("data-target")).removeClass("is-active");
-    $("html").removeClass("modal-open");
-});
